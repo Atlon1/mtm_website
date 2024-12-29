48ed2104c46c1af02909635551275e00b54a16e6
Atlon1 <maciej.szajstek@gmail.com>
2024-12-29 23:10:11 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/component/Hero";
+
+
+export default function Home() {
+    return (
+        <main>
+            <Hero />
+            <div className='h-[3000px]'></div>
+        </main>
+    );
+}
