d92e1bdb39281d0ab36f2f040b1690f73cceec87
Atlon1 <maciej.szajstek@gmail.com>
2025-01-03 21:00:15 +0100

    header prepere

--- component/Header.tsx
+++ component/Header.tsx
@@ -7,6 +7,8 @@ import Image from "next/image";
 import {useState, useEffect} from "react";
 import {MdMenu} from 'react-icons/md';
 import {GB, PL} from 'country-flag-icons/react/3x2'
+import {FaRegClock} from 'react-icons/fa'
+
 
 
 const Header = () => {
@@ -27,13 +29,26 @@ const Header = () => {
 
     return (
         <header
-            className={`${headerActive ? 'h-[100px]' : 'h-[124px]'} fixed max-w-[1920px] top-0 w-full bg-primary-200 h-[100px] transition-all z-50`}>
+            className={`${headerActive ? 'h-[130px]' : 'h-[144px]'} fixed max-w-[1920px] top-0 w-full bg-primary-200 h-[140px] transition-all z-50`}>
+            <div className='flex items-center justify-between border-accent border-b w-full p-2 text-white h-[30px] text-sm'>
+               <div className='flex flex-row gap-4 items-center'>
+                   <Link href='tel:+48690544645' >690544645</Link>
+                   <Link href='email:mtm.biuro.solution@gmial.com' >mtm.biuro.solution@gmial.com</Link>
+                   <FaRegClock className='text-accent'/><div> Pon - PT 8:00 - 22:00</div>
+               </div>
+                <div className='flex flex-row'>
+                    <div>facebook</div>
+                    <div>instagram</div>
+                    <div>youtube</div>
+                </div>
+
+            </div>
             <div className="container mx-a h-full flex items-center justify-between">
                 <Link href=''>
-                    <Image src={'/assets/img/Logo_bezTła.png'} width={200} height={70} alt='Logo'/>
+                    <Image src={'/assets/img/Logo_bezTła.png'} width={180} height={70} alt='Logo'/>
                 </Link>
                 <MobileNav
-                    containerStyle={`${headerActive ? 'top-[90px]' : 'top-[124px]'} ${openNav ? 'max-h-max pt-8 pb-10 border-t border-white/10' : 'max-h-0 pt-0 pb-0 overflow-hidden border-white/0'} text-white flex flex-col text-center gap-8 fixed bg-primary-200 w-full left-0 top-[124px] text-base uppercase font-medium transition-all xl:hidden`}/>
+                    containerStyle={`${headerActive ? 'top-[130px]' : 'top-[144px]'} ${openNav ? 'max-h-max pt-8 pb-10 border-t border-white/10' : 'max-h-0 pt-0 pb-0 overflow-hidden border-white/0'} text-white flex flex-col text-center gap-8 fixed bg-primary-200 w-full left-0 top-[124px] text-base uppercase font-medium transition-all xl:hidden`}/>
                 <Nav containerStyle='flex gap-4 text-white hidden xl:flex'/>
                 <div className='flex items-center gap-4'>
                     <div className='text-white flex items-center gap-2'>
