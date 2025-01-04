fd95f023be3b783f79ff9e9a175c197754b115dc
Atlon1 <maciej.szajstek@gmail.com>
2025-01-04 16:14:54 +0100

    header got time and contact

--- component/Header.tsx
+++ component/Header.tsx
@@ -8,6 +8,9 @@ import {useState, useEffect} from "react";
 import {MdMenu} from 'react-icons/md';
 import {GB, PL} from 'country-flag-icons/react/3x2'
 import {FaRegClock} from 'react-icons/fa'
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { BsTelephoneForward } from "react-icons/bs";
+import { FaFacebook, FaLinkedin, FaYoutube } from "react-icons/fa6";
 
 
 
@@ -29,17 +32,25 @@ const Header = () => {
 
     return (
         <header
-            className={`${headerActive ? 'h-[130px]' : 'h-[144px]'} fixed max-w-[1920px] top-0 w-full bg-primary-200 h-[140px] transition-all z-50`}>
-            <div className='flex items-center justify-between border-accent border-b w-full p-2 text-white h-[30px] text-sm'>
+            className={`${headerActive ? 'h-[100px]' : 'h-[130px]'} fixed max-w-[1920px] top-0 w-full bg-primary-200 h-[140px] transition-all z-50`}>
+            <div className={`${headerActive ? 'hidden' : 'h-[30px]'} md:flex hidden items-center justify-between border-accent border-b w-full p-2 text-white text-sm transition-all duration-300`}>
                <div className='flex flex-row gap-4 items-center'>
-                   <Link href='tel:+48690544645' >690544645</Link>
-                   <Link href='email:mtm.biuro.solution@gmial.com' >mtm.biuro.solution@gmial.com</Link>
+                   <BsTelephoneForward className='text-accent'/>
+                   <Link href='tel:+48690544645' className='hover:text-accent transition-all duration-300'>690544645</Link>
+                  <MdOutlineAlternateEmail className='text-accent'/>
+                   <Link href='mailto:mtm.biuro.solution@gmial.com' className='hover:text-accent transition-all duration-300' >mtm.biuro.solution@gmial.com</Link>
                    <FaRegClock className='text-accent'/><div> Pon - PT 8:00 - 22:00</div>
                </div>
-                <div className='flex flex-row'>
-                    <div>facebook</div>
-                    <div>instagram</div>
-                    <div>youtube</div>
+                <div className='flex flex-row gap-4 items-center'>
+                    <Link href='https://www.facebook.com/'><FaFacebook
+                        className='text-accent hover:text-white transition-all cursor-pointer duration-300'/>
+                    </Link>
+                    <Link href='https://www.linkedin.com/'>
+                        <FaLinkedin className='text-accent hover:text-white transition-all cursor-pointer duration-300'/>
+                    </Link>
+                    <Link href='https://www.youtube.com/'>
+                        <FaYoutube className='text-accent hover:text-white transition-all cursor-pointer duration-300'/>
+                    </Link>
                 </div>
 
             </div>
@@ -48,7 +59,7 @@ const Header = () => {
                     <Image src={'/assets/img/Logo_bezTła.png'} width={180} height={70} alt='Logo'/>
                 </Link>
                 <MobileNav
-                    containerStyle={`${headerActive ? 'top-[130px]' : 'top-[144px]'} ${openNav ? 'max-h-max pt-8 pb-10 border-t border-white/10' : 'max-h-0 pt-0 pb-0 overflow-hidden border-white/0'} text-white flex flex-col text-center gap-8 fixed bg-primary-200 w-full left-0 top-[124px] text-base uppercase font-medium transition-all xl:hidden`}/>
+                    containerStyle={`${headerActive ? 'top-[100px]' : 'top-[130px]'} ${openNav ? 'max-h-max pt-8 pb-10 border-t border-white/10' : 'max-h-0 pt-0 pb-0 overflow-hidden border-white/0'} text-white flex flex-col text-center gap-8 fixed bg-primary-200 w-full left-0 top-[124px] text-base uppercase font-medium transition-all xl:hidden`}/>
                 <Nav containerStyle='flex gap-4 text-white hidden xl:flex'/>
                 <div className='flex items-center gap-4'>
                     <div className='text-white flex items-center gap-2'>
