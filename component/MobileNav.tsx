b5a9dbf78724598607a04d5f623dbe259d4e0bcc
Atlon1 <maciej.szajstek@gmail.com>
2025-01-14 22:53:54 +0100

    upload lang

--- component/MobileNav.tsx
+++ component/MobileNav.tsx
@@ -2,25 +2,13 @@ import {linksPL} from "../translations/dataPL";
 import {linksENG} from "../translations/dataENG";
 import {Link as ScrollLink} from "react-scroll/modules";
 import {useMediaQuery} from "react-responsive";
-import {useEffect,useState} from "react";
 
 const MobileNav = ({containerStyle, closeNav}: { containerStyle: string , closeNav: any }) => {
     const isMobile = useMediaQuery({
         query: '(max-width: 640px)'
     })
 
-    const [links, setLinks] = useState<any[]>([])
-
-useEffect(()=>{
-    const lang = localStorage.getItem("lang") || "pl";
-    const newLinks = lang === "pl" ? linksPL : linksENG;
-
-    if (Array.isArray(newLinks)) {
-        setLinks(newLinks);
-    } else {
-        console.error("Links should be an array, but received:", typeof newLinks);
-    }
-},[])
+    const links = localStorage.getItem("lang") === "eng" ? linksENG : linksPL
 
 
     return (
